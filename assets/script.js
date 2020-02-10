5fb3d1a40d08269898c8a17eb8e5207e9ee4a60d
mikevampm323 <mike.vartanyan@yahoo.com>
2020-02-10 14:31:09 -0800

    bug fix

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var apiKey = '166a433c57516f51dfab1f7edaed8413';
-    var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather'
+    var currentWeatherURL = 'http://api.openweathermap.org/data/2.5/weather'
     var uvIndexURL = 'https://api.openweathermap.org/data/2.5/uvi'
     var fiveDayForecastURL = 'https://api.openweathermap.org/data/2.5/forecast'
     var uvIndex = $("#uv");
@@ -20,61 +20,6 @@ $(document).ready(function () {
         e.preventDefault();
         $("#icon").empty();
 
-        // var city = $('#city-search').val().trim();
-        // localStorage.setItem('history', city);
-        // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
-
-
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // })
-
-        //     .then(function (response) {
-
-        //         var longitude = response.coord.lon;
-        //         var latitude = response.coord.lat;
-
-        //         console.log(response);
-        //         console.log(longitude);
-        //         console.log(latitude);
-
-
-        //         $("#city").html("<h5>" + response.name + "</h5>");
-        //         $('#date').text(moment().format("dddd, MMMM Do YYYY"));
-
-        //         $("#wind").text("Wind Speed: " + response.wind.speed);
-        //         $("#humidity").text("Humidity: " + response.main.humidity + "%");
-        //         $("#temp").text("Temperature (F) " + response.main.temp);
-        //         var icon = $('<img>');
-
-        //         icon.attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
-        //         $('#icon').append(icon);
-
-        //         function createButton() {
-
-
-        //             var cities = localStorage.getItem('history');
-        //             var searchHistory = $("<button>" + cities + "</button>");
-        //             searchHistory.on("click", function () {
-
-
-        //             })
-
-
-
-        //             searchHistory.addClass("btn btn-light")
-        //             $(".history").append(searchHistory);
-
-        //         }
-
-
-        //         createButton();
-
-
-        //         $('#city-search').val("");
-
-        //     });
 
     });
 
